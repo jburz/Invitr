966c197d09d10dfb882756c7203f221880c4ea7f
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-10-24 11:44:09 -0600

    added eslint and initialized files

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log("Server listening on Port: " + PORT);
 });
-
