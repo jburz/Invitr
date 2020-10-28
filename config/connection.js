b47727e23a47e0ff8853e8b834ed2a6f216a85dd
Teknix <pandapaloo@gmail.com>
2020-10-27 19:23:28 -0600

    Added Database info and connection.js info

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,17 @@
 // Set up MYSQL connection
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "weddinginvite_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('Connected');
+});
+
+exports.connection = connection;
