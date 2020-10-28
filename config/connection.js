c7c3e09c100d1855ea7a94691b8f1ea2857e05de
Teknix <pandapaloo@gmail.com>
2020-10-27 20:24:31 -0600

    Changed the connection file some more to pass all of Travis.CI test

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 // Set up MYSQL connection
 const Connection = function () {
-    var mysql = require("mysql");
+    const mysql = require("mysql");
 
     const connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "",
+        password: "fishsticks420",
         database: "weddinginvite_db"
     });
 
-    connection.connect(function (err) {
-        if (err) throw err;
+    connection.connect((err) => {
+        if (err) { throw err };
         console.log("Connected");
     });
 };
