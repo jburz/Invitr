1f5d20487f835246f6bc0dfe1c136d3b3bb547d2
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-10-30 20:59:07 -0600

    added dasboard route to html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,9 @@
 module.exports = function (app) {
-    console.log("hit html-routes");
     app.get("/", (req, res) => {
         res.send("home page!");
     });
 
-
+    app.get("/dashboard", (req, res) => {
+        res.render("dashboard");
+    });
 };
