0317f264f484c6e18d52e6ce1493532b29f60373
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-11-02 17:42:18 -0700

    added ajax request to post a new guest, initialized server response

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,30 @@
 // Create routes and set up logic within those routes when required
-const GuestList = require("../models/guestlist.js");
-// var GuestLogin = require("../models/guestlogin.js");
+const db = require("../models");
 
 // Provide a list of all people currently on the guest list
 module.exports = function (app) {
     app.get("/api/all", (req, res) => {
-        GuestList.findAll({}).then((results) => {
+        db.GuestList.findAll({}).then((results) => {
             res.json(results);
         });
+    });
+    app.post("/api/newGuest", (req, res) => {
+        db.GuestList.create({ 
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            phone_number: req.body.phoneNumber,
+            street_address: req.body.streetAddress,
+            city_address: req.body.cityAddress,
+            zip_code: req.body.zipAddress,
+            state_address: req.body.stateAddress,
+            food_restriction: req.body.foodRestrictBool,
+            food_restriction_details: req.body.foodRestrictText,
+            additional_guests: req.body.additionalGuests,
+            email: req.body.email,
+            invited: req.body.invited,
+            rsvp: req.body.rsvp,
+            comment: req.body.comment
+        });
+        res.json(results);
     });
 };
